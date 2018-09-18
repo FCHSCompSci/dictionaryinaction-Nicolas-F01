92b255cd1f2f4ab50e3376e1c7398dda3f4b51ea
Nicolas Franchi <nicolas.franchi@stu.jefferson.kyschools.us>
2018-09-18 09:00:48 -0400

    added this feature

---
+++ project 2.py
@@ -0,0 +1,13 @@
+import random
+import sys
+import time
+
+def start_eft():
+    start= True
+    while star:
+    start_eft=input('Do you want to play Escape From Tarkov? Yes or No ')
+    if start_eft == "yes":
+        sleep.time(3)
+        print("You have started escape from Tarkov ")
+    elif start_eft == "no":
+        sys.exit
